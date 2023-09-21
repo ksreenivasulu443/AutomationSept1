7a3a7d68816e4b8f34d2ba62f07b8372e4abe4cc
MaheshDurgam9030 <122861464+MaheshDurgam9030@users.noreply.github.com>
2023-09-21 07:46:21 +0530

    hi

--- Datatyp/print_stmt.py
+++ Datatyp/print_stmt.py
@@ -1,9 +1,24 @@
-print("Hello world", end='-')
-#print()
-print("SReeni", end=',')
-print("damo")
-print('#'*40)
-print("sreeni","kattubadi")
-print("sreeni"+"kattubadi")
-a,b,c=10,12,13
-print(a,b,c)
+# print("Hello world", end='-')
+# #print()
+# print("SReeni", end=',')
+# print("damo")
+# print('#'*40)
+# print("sreeni","kattubadi")
+# print("sreeni"+"kattubadi")
+# a,b,c=10,12,13
+# print(a,b,c, sep=':')
+
+name='durga'
+salary=10000
+age=48
+print("{i} 's salary is {j} and his age is {k}".format(name,salary,age))
+print("{0} 's salary is {1} and his age is {2}".format(name,salary,age))
+print("{i} 's salary is {j} and his age is {k}".format(i=age,j=salary,k=name))
+
+# str ='TestHello'
+#
+# ls=[]
+# for i in str:
+#     if i not in ls:
+#         ls.append(i)
+#         print(i,str.count(i))
