93d7d3571003f6aa6157fa5a342b78fb24bfbbc8
MaheshDurgam9030 <122861464+MaheshDurgam9030@users.noreply.github.com>
2023-09-21 07:22:31 +0530

    hi

--- Datatyp/For_loop.py
+++ Datatyp/For_loop.py
@@ -66,14 +66,18 @@
 # print(type(a))
 # print(type(b))
 
-# x = 1
-# while x<10:
+# x = 100
+# while x>10:
 #     print(x)
 #     x = x+1
 
+x=10
 
-for i in range(1,10):
-    if i==5:
-        break
+while x>0:
+    print(x)
+    x=x-1
+
+
+
 
 
